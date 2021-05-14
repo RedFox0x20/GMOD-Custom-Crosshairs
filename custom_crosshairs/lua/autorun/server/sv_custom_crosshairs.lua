0e4cb2963d2964a76f6c4610c63195b1daa3a93c
Red (RedFox0x20) <tomwest0209@gmail.com>
2021-05-14 22:29:01 +0100

    Updates
    Update.sh is now x86_64 compatible branch
    
    Removed useless code and published

--- custom_crosshairs/lua/autorun/server/sv_custom_crosshairs.lua
+++ custom_crosshairs/lua/autorun/server/sv_custom_crosshairs.lua
@@ -1,6 +1,3 @@
-AddCSLuaFile("autorun/client/cvars.lua")
-AddCSLuaFile("autorun/client/ttt_custom_crosshairs.lua")
-
 resource.AddFile("materials/crosshair_cross.png")
 resource.AddFile("materials/crosshair_t.png")
 resource.AddFile("materials/crosshair_circle.png")
--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-~/.steam/steam/steamapps/common/GarrysMod/bin/gmad_linux custom_crosshairs
+~/.steam/steam/steamapps/common/GarrysMod/bin/linux64/gmad custom_crosshairs
 
 echo -e "Changes made $(date)\n" > /tmp/changes.txt
 vim + /tmp/changes.txt
 
-LD_PRELOAD=~/.steam/steam/steamapps/common/GarrysMod/bin/libsteam_api.so \
-		   ~/.steam/steam/steamapps/common/GarrysMod/bin/gmpublish_linux \
+LD_PRELOAD=~/.steam/steam/steamapps/common/GarrysMod/bin/linux64/libsteam_api.so \
+		   ~/.steam/steam/steamapps/common/GarrysMod/bin/linux64/gmpublish\
 		   update -id 2405650454 \
 		   -addon ./custom_crosshairs.gma \
 		   -changes "$(cat /tmp/changes.txt)"
